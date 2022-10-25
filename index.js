420052b027a23628529b6f0af44cfc201eb9993c
theocita <theocitaallen@gmail.com>
2022-10-25 19:48:41 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+    const endIndex = word.length - 1 - startIndex;
+      if (word[startIndex] !== word[endIndex]) {
+        return false;
+      }
+    }
+  
+   return true;
 }
 
 /* 
   Add your pseudocode here
+  iterate from the beginning of the string to the middle of the string
+  compare the letter we're iterating over to the corresponding letter at the end of the string
+    if the letters don't match, return false
+
+if we reach the middle, and all the letters match, return true
 */
 
 /*
   Add written explanation of your solution here
+  create a function where the word is a palindrome.
+  if the first letter and the last letter are the same, and the second letter and the seconf to last letter are the same and the rest letter correspond 
+  until we get to the middle. then, return true
 */
 
 // You can run `node index.js` to view these console logs
